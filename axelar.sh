f42ad69b8fb185bbe58797d9b2626009f1da8021
zulaiman133 <78168188+zulaiman133@users.noreply.github.com>
2024-04-16 20:03:33 +0800

    Update axelar.sh

--- axelar.sh
+++ axelar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://tm.axelar.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://rpc.starknet.lava.build/lava-referer-70f15651-4f36-4000-aac5-b6642eb53860/"
 
 OUTPUT_DIR="logs"
 
